d41d076f1b8d26eb988a50f164b95afccbc9621d
Benoît SÉRIE <bserie@evolix.fr>
2014-01-30 16:39:34 +0100

    Ameliorate report when creating or deleting incrementals.

--- crons/evobackup-inc.sh
+++ crons/evobackup-inc.sh
@@ -3,15 +3,25 @@
 
 . /etc/evobackup/conf.d/incrementals.cf
 
-start=$(date --rfc-3339=seconds)
+tmplog=$(mktemp --tmpdir=/tmp evobackup.tmplog.XXX)
+# Don't return *, if bash glob don't find files/dir.
+shopt -s nullglob
 
+# Search for incrementals to do.
 for client in ${CONFDIR}/*; do
+    start=$(date --rfc-3339=seconds)
     backupname=${client#/etc/evobackup/conf.d/incs/}
-    # hard copy everyday
-    echo -n "Hard copy of backup $backupname started at $start. " \
-        >> $LOGFILE
+    echo "Incrementals of $backupname started at $start. " \
+        >> $tmplog
     [[ ! -d ${INCDIR}/${backupname} ]] && mkdir -p ${INCDIR}/${backupname}
+    # Do the incrementals.
     cp -alx ${JAILDIR}/${backupname} ${INCDIR}/${backupname}/${DATEDIR}
     stop=$(date --rfc-3339=seconds)
-    echo -n "Hard copy of $backupname ended at $stop." >> $LOGFILE
-done | tee -a $LOGFILE | mailx -s "[info] EvoBackup report of creating incrementals" $MAIL_TO
+    echo "Incrementals of $backupname ended at $stop." >> $tmplog
+done
+# Save tmplog to global log.
+cat $tmplog >> $LOGFILE
+# Send mail report.
+< $tmplog mailx -s "[info] EvoBackup report of creating incrementals" $MAIL_TO
+# Cleaning.
+rm $tmplog
